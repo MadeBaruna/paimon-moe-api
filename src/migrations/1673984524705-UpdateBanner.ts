4a7f3e4118b5daeb8b62f28ec49606f8fbbe791c
Made Baruna <made.setia@gmail.com>
2023-01-18 03:49:28 +0800

    Update banner

---
+++ src/migrations/1673984524705-UpdateBanner.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { Banner } from '../entities/banner';
+
+const banners = {
+  characters: {
+    name: 'Caution in Confidence',
+    start: '2023-01-18 06:00:00',
+    end: '2023-02-07 17:59:59',
+    id: 300042,
+  },
+  weapons: {
+    name: 'Epitome Invocation',
+    start: '2023-01-18 06:00:00',
+    end: '2023-02-07 17:59:59',
+    id: 400041,
+  },
+};
+
+export class UpdateBanner1673984524705 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const newCharacterBanner = banners.characters;
+    const characterBanner = new Banner();
+    characterBanner.id = newCharacterBanner.id;
+    characterBanner.type = 'characters';
+    characterBanner.name = newCharacterBanner.name;
+    characterBanner.start = `${newCharacterBanner.start}+8`;
+    characterBanner.end = `${newCharacterBanner.end}+8`;
+
+    const newWeaponBanner = banners.weapons;
+    const weaponBanner = new Banner();
+    weaponBanner.id = newWeaponBanner.id;
+    weaponBanner.type = 'weapons';
+    weaponBanner.name = newWeaponBanner.name;
+    weaponBanner.start = `${newWeaponBanner.start}+8`;
+    weaponBanner.end = `${newWeaponBanner.end}+8`;
+
+    await queryRunner.manager.save([characterBanner, weaponBanner]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.delete(Banner, [300042, 400041]);
+  }
+}
