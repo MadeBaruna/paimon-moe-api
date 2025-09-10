75cd255bdf7ea8fe4cfdea720825d7ffb7fc72f6
Made Baruna <made.setia@gmail.com>
2025-09-10 11:12:14 +0800

    Update banners

--- src/routes/wish.ts
+++ src/routes/wish.ts
@@ -23,8 +23,8 @@ import { WishSummaryRequest } from '../types/wishSummaryRequest';
 import { WishSummaryLuckRequest } from '../types/wishSummaryLuckRequest';
 import { wishSummary, wishSummaryLuck4, wishSummaryLuck5, wishSummaryWinRateOff4, wishSummaryWinRateOff5 } from '../stores/wishSummary';
 
-const LATEST_CHARACTER_BANNER = 300087;
-const LATEST_WEAPON_BANNER = 400086;
+const LATEST_CHARACTER_BANNER = 300088;
+const LATEST_WEAPON_BANNER = 400087;
 
 export default async function (server: FastifyInstance): Promise<void> {
   server.get(
