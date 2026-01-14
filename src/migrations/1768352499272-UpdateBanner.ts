9b97f81c10762be8c7a0c88a9790b548b0122466
Made Baruna <made.setia@gmail.com>
2026-01-14 09:04:25 +0800

    Update banners

---
+++ src/migrations/1768352499272-UpdateBanner.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { Banner } from '../entities/banner';
+
+const banners = {
+  characters: {
+    name: 'Somnias a Luna',
+    start: '2026-01-14 06:00:00',
+    end: '2026-02-03 17:59:00',
+    id: 300094,
+  },
+  weapons: {
+    name: 'Epitome Invocation',
+    start: '2026-01-14 06:00:00',
+    end: '2026-02-03 17:59:00',
+    id: 400093,
+  },
+};
+
+export class UpdateBanner1768352499272 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const newCharacterBanner = banners.characters;
+    const characterBanner = new Banner();
+    characterBanner.id = newCharacterBanner.id;
+    characterBanner.type = 'characters';
+    characterBanner.name = newCharacterBanner.name;
+    characterBanner.start = `${newCharacterBanner.start}+8`;
+    characterBanner.end = `${newCharacterBanner.end}+8`;
+
+    const newWeaponBanner = banners.weapons;
+    const weaponBanner = new Banner();
+    weaponBanner.id = newWeaponBanner.id;
+    weaponBanner.type = 'weapons';
+    weaponBanner.name = newWeaponBanner.name;
+    weaponBanner.start = `${newWeaponBanner.start}+8`;
+    weaponBanner.end = `${newWeaponBanner.end}+8`;
+
+    await queryRunner.manager.save([characterBanner, weaponBanner]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.delete(Banner, [300094, 400093]);
+  }
+}
--- src/routes/wish.ts
+++ src/routes/wish.ts
@@ -23,8 +23,8 @@ import { WishSummaryRequest } from '../types/wishSummaryRequest';
 import { WishSummaryLuckRequest } from '../types/wishSummaryLuckRequest';
 import { wishSummary, wishSummaryLuck4, wishSummaryLuck5, wishSummaryWinRateOff4, wishSummaryWinRateOff5 } from '../stores/wishSummary';
 
-const LATEST_CHARACTER_BANNER = 300093;
-const LATEST_WEAPON_BANNER = 400092;
+const LATEST_CHARACTER_BANNER = 300094;
+const LATEST_WEAPON_BANNER = 400093;
 
 export default async function (server: FastifyInstance): Promise<void> {
   server.get(
